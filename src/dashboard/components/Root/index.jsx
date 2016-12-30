ee578686512db525e86b58452b25ff61e5fdc4ee
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-12-30 17:04:28 +0100

    Done coming soon version

--- src/dashboard/components/Root/index.jsx
+++ src/dashboard/components/Root/index.jsx
@@ -1,18 +1,40 @@
 import React from 'react';
-import * as deps from '../../deps';
 
-const Content = () => (
-  <div>
-    <h1>I am a Worona package</h1>
-    <div>And this is my content.</div>
-  </div>
+const Fbia = () => (
+  <section className="hero">
+    <div className="container">
+      <div className="hero-body">
+        <h1 className="title">Facebook Instant Articles</h1>
+        <h2 className="subtitle">
+          A new way for any publisher to create fast, interactive articles on Facebook.
+        </h2>
+        <br />
+        <div className="columns">
+          <div className="column is-half is-offset-one-quarter">
+            <article className="message is-info">
+              <div className="message-header">
+                Coming Soon
+              </div>
+              <div className="message-body">
+                We are working to integrate more channels so you can distribute your content easily on mobile.
+                <br /><br />
+                If you want to learn more
+                <a target="_blank" rel="noopener noreferrer" href="https://instantarticles.fb.com/faqs/">the FAQs</a> or their <a href="https://instantarticles.fb.com/getting-started/" rel="noopener noreferrer" target="_blank">Getting started</a> guide.
+                about Facebook Instant Articles you can take a look to their <a target="_blank" rel="noopener noreferrer" href="https://instantarticles.fb.com">official site</a>,
+                <br /><br /><br />
+                <p className="control">
+                  <label className="checkbox" htmlFor="notifyMeFbia">
+                    <input id="notifyMeFbia" type="checkbox" />
+                    &nbsp;<strong>Please notify me when you launch.</strong>
+                  </label>
+                </p>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 );
 
-export default () => {
-  const RootContainer = deps.elements.RootContainer;
-  return (
-    <RootContainer mobilePreview>
-      <Content />
-    </RootContainer>
-  );
-};
+export default Fbia;
--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,9 +1,5 @@
 import * as components from './components';
-import * as sagas from './sagas';
-import * as deps from './deps';
 
 export {
   components,
-  sagas,
-  deps,
 };
